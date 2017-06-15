9d0e60cfec2d1ac5a46ea9664bb2e4ac3c373abe
jo <jotech@users.noreply.github.com>
2017-06-15 15:25:38 +0200

    updated basic stat figure

--- mods_stat.R
+++ mods_stat.R
@@ -11,14 +11,16 @@ cele_stat <- data.frame()
 # basic comparison
 ex_jul <- findExchReact(cele_jul)
 ex_con <- findExchReact(cele_con)
+blocked_jul <- sum(blockedReact(changeBounds(cele_jul, ex_jul, lb = -1000, ub = 1000)))
+blocked_con <- sum(blockedReact(changeBounds(cele_con, ex_con, lb = -1000, ub = 1000)))
 cele_stat <- rbind(cele_stat, 
                    data.frame("model"="juliane", "reactions"=ncol(S(cele_jul)), 
                               "metabolites"=nrow(S(cele_jul)), "genes"=length(cele_jul@allGenes),
-                              "exchanges"=ex_jul@react_num))
+                              "exchanges"=ex_jul@react_num, "blocked"=blocked_jul))
 cele_stat <- rbind(cele_stat, 
                    data.frame("model"="consensus", "reactions"=ncol(S(cele_con)), 
                               "metabolites"=nrow(S(cele_con)), "genes"=length(cele_con@allGenes),
-                              "exchanges"=ex_con@react_num))
+                              "exchanges"=ex_con@react_num, "blocked"=blocked_con))
 
 cele_melt <- melt(cele_stat, id.vars = "model")
 
@@ -26,8 +28,9 @@ ggplot(cele_melt, aes(x=variable, y=value, fill=model)) +
   geom_bar(stat="identity", position = position_dodge(width = 0.9)) + xlab("") + ylab("")
 #ggsave("~/uni/cele_mods/img/basic_cmp.pdf")
 
-blocked_jul <- sum(blockedReact(cele_jul))
-blocked_con <- sum(blockedReact(cele_con))
+
+
+# alternative calculation of blocked reactions
 
 fva_jul <- fluxVar(cele_jul)
 fva_con <- fluxVar(cele_con)
